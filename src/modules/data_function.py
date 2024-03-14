73758bdf69ab1088749c4d6d1f1e49091b4f6780
Christophe Houbron <christof.houbron@gmail.com>
2024-03-14 15:12:39 +0100

    Modification of tests file to use new split function of data_function and library_design.py (#19)

--- src/modules/data_function.py
+++ src/modules/data_function.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 from src.models.library import Library
 
@@ -15,8 +16,12 @@ def load_parameters(json_path: str) -> dict[str, str | int]:
     """
     with open(json_path, mode="r", encoding="UTF-8") as file:
         input_param = json.load(file)
-        input_param['end_lib'] = input_param['start_lib'] + (input_param['nbr_loci_total'] * input_param['resolution'])
-        input_param['genomic_path'] = input_param['chromosome_folder'] + os.sep + input_param['chromosome_file']
+        input_param["end_lib"] = input_param["start_lib"] + (
+            input_param["nbr_loci_total"] * input_param["resolution"]
+        )
+        input_param["genomic_path"] = (
+            input_param["chromosome_folder"] + os.sep + input_param["chromosome_file"]
+        )
         return input_param
 
 
@@ -32,12 +37,12 @@ def universal_primer_format(path: str) -> dict[str, list[str]]:
     primer_univ = {}
     with open(path, mode="r", encoding="utf-8") as file:
         for line in file:
-            data = line.replace('\n','').split(',')
+            data = line.replace("\n", "").split(",")
             primer_univ[data[0]] = [item for item in data[1:]]
     return primer_univ
 
 
-def bcd_rt_format(path : str) -> list[list[str]]:
+def bcd_rt_format(path: str) -> list[list[str]]:
     """Function for opening, formatting and storing barcode or RT sequences.
 
     Args:
@@ -48,14 +53,14 @@ def bcd_rt_format(path : str) -> list[list[str]]:
     """
 
     bcd_rt_list = []
-    with open(path, mode='r', encoding='utf-8') as file:
+    with open(path, mode="r", encoding="utf-8") as file:
         for line in file:
-            data = line.replace('\n', '').split(',')
+            data = line.replace("\n", "").split(",")
             bcd_rt_list.append(data)
         return bcd_rt_list
 
 
-def seq_genomic_format(path : str) -> list[int , int, str]:
+def seq_genomic_format(path: str) -> list[int, int, str]:
     """Function for opening, formatting and storing genomic sequences.
 
     Args:
@@ -65,9 +70,9 @@ def seq_genomic_format(path : str) -> list[int , int, str]:
         list[int, int, str]: sequence of genomic DNA with coordinates
     """
     seq_genomic_list = []
-    with open(path, mode='r', encoding='UTF-8') as file :
-        for line in file :
-            data = line.split('\t')
+    with open(path, mode="r", encoding="UTF-8") as file:
+        for line in file:
+            data = line.split("\t")
             seq_genomic_list.append([int(data[1]), int(data[2]), data[3]])
     return seq_genomic_list
 
@@ -84,8 +89,10 @@ def result_details_file(path_result_folder: str, library: Library) -> None:
     result_details = path_result_folder + os.sep + "1_Library_details.txt"
     with open(result_details, mode="w", encoding="UTF-8") as file:
         for locus in library.loci_list:
-            file.write(f"Chromosome: {locus.chr_name} Locus_N°{locus.locus_n}\
-Start:{locus.start_seq} End:{locus.end_seq} Bcd_locus:{locus.bcd_locus}\n")
+            file.write(
+                f"Chromosome: {locus.chr_name} Locus_N°{locus.locus_n}\
+Start:{locus.start_seq} End:{locus.end_seq} Bcd_locus:{locus.bcd_locus}\n"
+            )
             for seq in locus.seq_probe:
                 file.write(seq + "\n")
 
@@ -116,15 +123,24 @@ def library_summary_file(path_result_folder: str, library: Library) -> None:
         library (Library):
             library containing all information and sequences
     """
-    summary = path_result_folder + os.sep + "3_Library_summary.csv"
-    with open(summary, mode="w", encoding="UTF-8") as file:
-        file.write("Chromosome,Locus_N°,Start,End,Region size, Barcode,PU.Fw,PU.Rev,Nbr_Probes\n")
-        for locus in library.loci_list:
-            file.write(f"{locus.chr_name},{locus.locus_n},{locus.start_seq},\
-    {locus.end_seq},{locus.end_seq - locus.start_seq},{locus.bcd_locus},{locus.primers_univ[0]},\
-    {locus.primers_univ[2]},{len(locus.seq_probe)}\n")
 
-def save_parameters (path_result_folder: str, out_parameters: dict[str, str | int]) -> None:
+    path = Path(path_result_folder)
+    summary = path.joinpath("3_Library_summary.csv")
+    with open(summary, mode="w", encoding="UTF-8") as file:
+        file.write(
+            "Chromosome,Locus_N°,Start,End,Region size, Barcode,PU.Fw,PU.Rev,Nbr_Probes\n"
+        )
+        for locus in library.loci_list:
+            file.write(
+                f"{locus.chr_name},{locus.locus_n},{locus.start_seq},\
+{locus.end_seq},{locus.end_seq - locus.start_seq},{locus.bcd_locus},{locus.primers_univ[0]},\
+{locus.primers_univ[2]},{len(locus.seq_probe)}\n"
+            )
+
+
+def save_parameters(
+    path_result_folder: str, out_parameters: dict[str, str | int]
+) -> None:
     """Save all parameters used to design librairy in a json file.
 
     Args:
@@ -137,4 +153,6 @@ def save_parameters (path_result_folder: str, out_parameters: dict[str, str | in
     with open(parameters_file_path, mode="w", encoding="UTF-8") as file:
         json.dump(out_parameters, file, indent=4)
 
-    print(f"All files concerning your library design are saved in {path_result_folder}/")
+    print(
+        f"All files concerning your library design are saved in {path_result_folder}/"
+    )
