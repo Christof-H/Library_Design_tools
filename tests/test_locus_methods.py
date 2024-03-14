26244a4454c9ffc66f5bd5c0a7ae75e6fcfa4695
Christophe Houbron <christof.houbron@gmail.com>
2024-03-14 15:31:02 +0100

    split pytest.fixture multiple returns in single return with multiple pystest.fixture (#12)

--- tests/test_locus_methods.py
+++ tests/test_locus_methods.py
@@ -15,7 +15,11 @@ def locus():
         "CCAGTCCAGAGGTGTCCCTAC",
     ]
     locus = Locus(resolution=20000, nbr_probe_by_locus=100, primers_univ=primer_univ)
+    return locus
 
+
+@pytest.fixture
+def sequences():
     start_seq = list(range(8000, 80000, 50))
     end_seq = list(range(8030, 80030, 50))
     seq = [
@@ -25,26 +29,20 @@ def locus():
     seq_list = []
     for start, end, seq in zip(start_seq, end_seq, seq):
         seq_list.append([start, end, seq])
+    return seq_list
 
-    return locus, seq_list
-
-
-def test_recover_genomic_seq_locus_length_strategy(locus):
-    locus, seq_list = locus
+def test_recover_genomic_seq_locus_length_strategy(locus, sequences):
     locus.design_type = "locus_length"
-    final_seq, start, end = locus.recover_genomic_seq(2, 3, 10000, seq_list)
+    final_seq, start, end = locus.recover_genomic_seq(2, 3, 10000, sequences)
     assert len(final_seq) == 100 and start == 30000 and end == 50000
 
 
-def test_recover_genomic_seq_nbr_probes_strategy(locus):
-    locus, seq_list = locus
+def test_recover_genomic_seq_nbr_probes_strategy(locus, sequences):
     locus.design_type = "nbr_probes"
-    final_seq, start, end = locus.recover_genomic_seq(2, 3, 10000, seq_list)
+    final_seq, start, end = locus.recover_genomic_seq(2, 3, 10000, sequences)
     assert len(final_seq) == 100 and start == 13000 and end == 17980
 
 
-def test_check_nbr_probes_overtaking(locus):
-    print(os.getcwd())
-    locus, seq_list = locus
-    seq_list = seq_list[:400]
+def test_check_nbr_probes_overtaking(locus, sequences):
+    seq_list = sequences[:400]
     assert len(locus.check_nbr_probes(seq_list)) == locus.nbr_probe_by_locus
