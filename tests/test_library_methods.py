b36d02b49873de8c75b290afd56a58b486faca0a
Christophe Houbron <christof.houbron@gmail.com>
2024-03-14 15:31:02 +0100

    split pytest.fixture multiple returns in single return with multiple pystest.fixture (#12)

--- tests/test_library_methods.py
+++ tests/test_library_methods.py
@@ -6,7 +6,7 @@ from src.models.locus import Locus
 
 
 @pytest.fixture
-def setup():
+def parameters():
     # Set parameters for Library instantiation
     parameters = {
         "chromosome_file": "ch3L.bed",
@@ -15,9 +15,11 @@ def setup():
         "max_diff_percent": 10,
         "design_type": "locus_length",
     }
-    # Set simply library without locus, seq_probe
-    library_empty = Library(parameters)
+    return parameters
 
+
+@pytest.fixture
+def sequences():
     # design sequence randomly with coordinates
     start_seq = list(range(8000, 80000, 50))
     end_seq = list(range(8030, 80030, 50))
@@ -28,56 +30,61 @@ def setup():
     seq_list = []
     for start, end, seq in zip(start_seq, end_seq, seq):
         seq_list.append([start, end, seq])
+    return seq_list
 
+
+@pytest.fixture
+def library_empty(parameters):
+    # Set simply library without locus, seq_probe
+    return Library(parameters)
+
+
+@pytest.fixture
+def library_filled(parameters, sequences):
     # Set library with locus and seq_probe
-    library_with_loci = Library(parameters)
+    library = Library(parameters)
 
     seq1 = [8733, 8763, "GATAGATAGCATCATCATCTACTATCATCTATCAT"]
     locus1 = Locus(
         ["BB297.Fw", "GACTGGTACTCGCGTGACTTG", "BB299.Rev", "CCAGTCCAGAGGTGTCCCTAC"]
     )
-    locus1.seq_probe = [x[2] for x in seq_list[:30]]
+    locus1.seq_probe = [x[2] for x in sequences[:30]]
     locus2 = Locus(
         ["BB297.Fw", "GACTGGTACTCGCGTGACTTG", "BB299.Rev", "CCAGTCCAGAGGTGTCCCTAC"]
     )
-    locus2.seq_probe = [x[2] for x in seq_list[30:60]]
+    locus2.seq_probe = [x[2] for x in sequences[30:60]]
     locus2.seq_probe.append(seq1[2])
-    library_with_loci.add_locus(locus1)
-    library_with_loci.add_locus(locus2)
+    library.add_locus(locus1)
+    library.add_locus(locus2)
+    return library
 
-    return seq_list, library_empty, library_with_loci
 
-
-def test_reduce_list_seq_type_locus_length(setup):
-    sequences, library, _ = setup
-    library.design_type = "locus_length"
-    seq_list_reduced = library.reduce_list_seq(
+def test_reduce_list_seq_type_locus_length(sequences, library_empty):
+    library_empty.design_type = "locus_length"
+    seq_list_reduced = library_empty.reduce_list_seq(
         sequences, resolution=1000, nbr_probe_by_locus=20
     )
     assert len(seq_list_reduced) == 100
 
 
-def test_reduce_list_seq_type_nbr_probes(setup):
-    sequences, library, _ = setup
-    library.design_type = "nbr_probes"
-    seq_list_reduced = library.reduce_list_seq(
+def test_reduce_list_seq_type_nbr_probes(sequences, library_filled):
+    library_filled.design_type = "nbr_probes"
+    seq_list_reduced = library_filled.reduce_list_seq(
         sequences, resolution=1000, nbr_probe_by_locus=30
     )
     assert len(seq_list_reduced) == 150
 
 
-def test_check_length_seq_diff_check_returns_values(setup):
-    sequences, _, library_with_loci = setup
-    min, max, diff_bp, diff_percent = library_with_loci.check_length_seq_diff()
+def test_check_length_seq_diff_check_returns_values(sequences, library_filled):
+    min, max, diff_bp, diff_percent = library_filled.check_length_seq_diff()
     assert (
         min == 30 and max == 35 and diff_bp == 5 and diff_percent == 14.285714285714292
     )
 
 
-def test_completion_without_threshold_exceeded(setup, capsys):
-    sequences, _, library_with_loci = setup
-    library_with_loci.max_diff_percent = 20
-    library_with_loci.completion(14, 35)
+def test_completion_without_threshold_exceeded(sequences, library_filled, capsys):
+    library_filled.max_diff_percent = 20
+    library_filled.completion(14, 35)
     captured_stdout = capsys.readouterr().out
     assert (
         captured_stdout
@@ -85,9 +92,8 @@ def test_completion_without_threshold_exceeded(setup, capsys):
     )
 
 
-def test_completion_with_threshold_exceeded(setup, capsys):
-    sequences, _, library_with_loci = setup
-    library_with_loci.completion(14, 135)
+def test_completion_with_threshold_exceeded(sequences, library_filled, capsys):
+    library_filled.completion(14, 135)
     captured_stdout = capsys.readouterr().out
     assert (
         captured_stdout == "-" * 70 + "\n" + "Completion finished\n" + "-" * 70 + "\n"
