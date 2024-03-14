9132f5c6e6e8028a8ade3d052d04c917115d3162
Christophe Houbron <christof.houbron@gmail.com>
2024-03-14 09:27:16 +0100

    import error correction : data_function as df (#11)

--- tests/tests_data_function.py
+++ tests/tests_data_function.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.modules.data_import_function import bcd_rt_format, universal_primer_format, seq_genomic_format
+import src.modules.data_function as df
 
 
 @pytest.fixture
@@ -15,23 +15,23 @@ def file_path():
 
 def test_bcd_rt_format_rt_output(file_path):
     expected_output_rt_0 = ['revMer1', 'caccgacgtcgcatagaacg']
-    list_bcd_rt = bcd_rt_format(file_path['rt_file_path'])
+    list_bcd_rt = df.bcd_rt_format(file_path['rt_file_path'])
     assert list_bcd_rt[0] == expected_output_rt_0
 
 
 def test_bcd_rt_format_bcd_output(file_path):
     expected_output_bcd_0 = ['Bcd_001', 'GCTATCGTTCGTTCGAGGCC']
-    list_bcd_rt = bcd_rt_format(file_path['bcd_file_path'])
+    list_bcd_rt = df.bcd_rt_format(file_path['bcd_file_path'])
     assert list_bcd_rt[0] == expected_output_bcd_0
 
 
 def test_universal_primer_format_output(file_path):
     expected_output_univ_primer_1 = ['BB297.Fw', 'GACTGGTACTCGCGTGACTTG', 'BB299.Rev', 'CCAGTCCAGAGGTGTCCCTAC']
-    output_univ_primer_1 = universal_primer_format(file_path['univ_primer']).get('primer1')
+    output_univ_primer_1 = df.universal_primer_format(file_path['univ_primer']).get('primer1')
     assert expected_output_univ_primer_1 == output_univ_primer_1
 
 def test_seq_genomic_format_output_type(file_path):
-    seq_genomic_output_0 = seq_genomic_format(file_path['exemple_genomic_seq'])[0]
+    seq_genomic_output_0 = df.seq_genomic_format(file_path['exemple_genomic_seq'])[0]
     assert (isinstance(seq_genomic_output_0, list)
             and isinstance(seq_genomic_output_0[0], int)
             and isinstance(seq_genomic_output_0[1], int)
