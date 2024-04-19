621dfdbfffb142110deb183a555ffe3f2c53a64f
Christof-H <christof.houbron@gmail.com>
2024-04-19 18:05:50 +0200

    separation of CLI and GUI and grouping the library design process in a lib_design function

--- src/library_design.py
+++ src/library_design.py
@@ -27,64 +27,10 @@ It is possible to embed multiple libraries within one oligopool by using differe
 universal primers.
 
 """
-import os
-import copy
 from pathlib import Path
 
-import datetime as dt
-from argparse import ArgumentParser
-
-import core.data_function as df
 from core.args import parse_arguments, check_args
-from core.function import display_locus_info
-from models.library import Library
-from models.locus import Locus
-from models.invalidNbrLocusException import InvalidNbrLocusException
-
-
-# ---------------------------------------------------------------------------------------------
-#                                       Functions
-# ---------------------------------------------------------------------------------------------
-def print_dashline():
-    """print a dash line"""
-    print("-" * 70)
-
-
-def check_locus_rt_bcd(
-    parameters: dict[str, str | int], bcd_rt_list: list[list[str]]
-) -> None:
-    """Check that there are enough barcodes or RTs for the total number of loci.
-    Args:
-        parameters (dict[str, str | int]):
-            parameters in a dictionary
-        bcd_rt_list (list[list[str]]):
-            a list of barcodes or RT in format [name, sequence]
-
-    Raises:
-        InvalidNbrLocusException: If the number of available barcodes or RTs is insufficient
-            compared to the total number of loci.
-    """
-    type_bcdrt = "barcodes" if parameters["bcd_rt_file"] == "Barcodes.csv" else "RTs"
-    if len(bcd_rt_list) < parameters["nbr_loci_total"]:
-        raise InvalidNbrLocusException(
-            nbr_locus=parameters["nbr_loci_total"],
-            nbr_bcd_rt=len(bcd_rt_list),
-            type_bcd_rt=type_bcdrt,
-        )
-
-
-def print_sample(
-    list_seq_genomic: list[int, int, str],
-    bcd_rt_list: list[list[str]],
-    primer_univ_list: dict[str, list[str]],
-) -> None:
-    print_dashline()
-    print("list_seq_genomic =", list_seq_genomic[0])
-    print_dashline()
-    print("bcd_RT =", bcd_rt_list[:2])
-    print_dashline()
-    print("primer_univ = ", "primer1 =", primer_univ_list["primer1"])
-    print_dashline()
+from core.lib_design import lib_design
 
 
 def main():
@@ -97,161 +43,15 @@ def main():
 
     args = parse_arguments()
     check_args(args)
-    # ---------------------------------------------------------------------------------------------
-    #                               Importing library parameters
-    # ---------------------------------------------------------------------------------------------
 
-    # Retrieving parameters from the input_parameters.json file
-    json_path = args.parameters
-    parameters = df.load_parameters(json_path, src_folder)
-
-    # ---------------------------------------------------------------------------------------------
-    #                                   Creating result folder
-    # ---------------------------------------------------------------------------------------------
-    result_folder = args.output.joinpath("Library_Design_Results")
-    if not result_folder.exists():
-        result_folder.mkdir()
-
-    # ---------------------------------------------------------------------------------------------
-    #                           Formatting and storage of sequences
-    #               (primers, TRs, barcodes, genomics) in corresponding variables
-    # ---------------------------------------------------------------------------------------------
-
-    # Opening and formatting barcodes or RTs in the bcd_RT variable:
-    bcd_rt_list = df.bcd_rt_format(parameters["bcd_rt_path"])
-
-    # Opening and formatting the coordinates and genomic sequences of in the list_seq_genomic variable :
-    list_seq_genomic = df.seq_genomic_format(parameters["genomic_path"])
-
-    # Opening and formatting universal primers in the primer_univ variable : :
-    primer_univ_list = df.universal_primer_format(parameters["primer_univ_path"])
-
-    print_sample(list_seq_genomic, bcd_rt_list, primer_univ_list)
-
-    # ---------------------------------------------------------------------------------------------
-    #       Check the number of loci against the number of RTs or barcodes available
-    # ---------------------------------------------------------------------------------------------
-    check_locus_rt_bcd(parameters, bcd_rt_list)
-
-    # ---------------------------------------------------------------------------------------------
-    #                               Filling locus information
-    #           (Primers Univ, start coordinates, end coordinates, DNA genomic sequences)
-    # ---------------------------------------------------------------------------------------------
-
-    # Search for the desired universal primers
-    primer = [
-        primer_univ_list[key]
-        for key, values in primer_univ_list.items()
-        if key == parameters["primer_univ"]
-    ]
-    primer = primer[0]
-
-    # Create and fill Library object with the different parameters
-    library = Library(parameters)
-
-    # Reduce genomic sequence according to loci coordinates or probe number
-    list_seq_genomic_reduced = library.reduce_list_seq(
-        list_seq_genomic,
-        resolution=parameters["resolution"],
-        nbr_probe_by_locus=parameters["nbr_probe_by_locus"],
-    )
-
-    # Fill the Library object with all the Locus
-    for i in range(1, library.nbr_loci_total + 1):
-        locus = Locus(
-            primers_univ=primer,
-            locus_n=i,
-            chr_name=library.chromosome_name,
-            resolution=parameters["resolution"],
-            nbr_probe_by_locus=parameters["nbr_probe_by_locus"],
-            design_type=parameters["design_type"],
-        )
-        list_seq, start, end = locus.recover_genomic_seq(
-            i,
-            parameters["nbr_loci_total"],
-            parameters["start_lib"],
-            list_seq_genomic_reduced,
-        )
-        locus.start_seq = start
-        locus.end_seq = end
-        # locus.primers_univ=primer,
-        locus.seq_probe = list_seq
-        library.add_locus(locus)
-
-    # Display of a locus as an example
-    print_dashline()
-    print("Locus exemple :")
-    print(library.loci_list[0])
-
-    # Sequences for barcodes/RTs added to primary probes according to locus
-    library.add_rt_bcd_to_primary_seq(bcd_rt_list, parameters)
-
-    # Sequences for universal primers added to the primary probes at each end
-    library.add_univ_primer_each_side()
-
-    # Display example of a final primary probe sequence
-    print_dashline()
-    print("example of a primary probe sequence :")
-    print_dashline()
-    print(library.loci_list[0].seq_probe[0])
-
-    # ---------------------------------------------------------------------------------------------
-    #                               Checking and completion
-    # ---------------------------------------------------------------------------------------------
-
-    # Checking primary probes length for all Locus
-    min_length, max_length, diff_nbr, diff_percentage = library.check_length_seq_diff()
-    print_dashline()
-    print("Result of probes checking :")
-    print(f"minimum size for all probes combined : {min_length}")
-    print(f"maximum size for all probes combined : {max_length}")
-    print(f"difference in size : {diff_percentage:.1f}%")
-
-    # If there is a significant difference in size between the primary probes of all the Locus,
-    # completion primary probes too small to standardise the length of the oligo-pool
-    # ATTENTION: 3' completion of the sequence
-    library.completion(diff_percentage, max_length)
-
-    # ---------------------------------------------------------------------------------------------
-    #                           Create result folder
-    # ---------------------------------------------------------------------------------------------
-
-    # Creation of a dated file to differentiate between the different libraries designed
-    date_now = dt.datetime.now().strftime("%Y%m%d_%H%M")
-    path_result_folder = result_folder.joinpath(date_now)
-    path_result_folder.mkdir()
-
-    # ---------------------------------------------------------------------------------------------
-    #                           Display probes/length by locus
-    # ---------------------------------------------------------------------------------------------
-    list_info = library.recover_loci_probes_length_info()
-    display_locus_info(
-        list_info,
-        path_result_folder,
-        parameters["design_type"],
-        parameters["resolution"],
-        parameters["nbr_probe_by_locus"],
-    )
-
-    # ---------------------------------------------------------------------------------------------
-    #                           Writing the various results files
-    # ---------------------------------------------------------------------------------------------
-
-    # writing the file with detailed information (information for each locus and sequence)
-    df.result_details_file(path_result_folder, library)
-
-    # writing the file with all primary probe sequences for all locus (without spaces)
-    df.full_sequences_file(path_result_folder, library)
-
-    # writing file with summary information (without sequence) in the form of a table
-    df.library_summary_file(path_result_folder, library)
-
-    # Retrieve the parameters used to design the library
-    output_parameters = copy.deepcopy(parameters)
-    output_parameters["Script_Name"] = "library_design.py"
-
-    # Write library parameters in the 4-OutputParameters.json file
-    df.save_parameters(path_result_folder, output_parameters)
+    if not args.nogui:
+        json_path = args.parameters
+        output_folder = args.output
+        lib_design(json_path, output_folder, src_folder)
+        print("Lancement du design avec CLI")
+    else:
+        # gui_design(json_path, output_folder, src_folder)
+        print("Lancement du design avec GUI")
 
 
 if __name__ == "__main__":
--- src/models/locus.py
+++ src/models/locus.py
@@ -1,27 +1,52 @@
 import random
 
+from models.invalidNbrLocusException import InvalidNbrLocusException
+
+
+def check_locus_rt_bcd(
+    parameters: dict[str, str | int], bcd_rt_list: list[list[str]]
+) -> None:
+    """Check that there are enough barcodes or RTs for the total number of loci.
+    Args:
+        parameters (dict[str, str | int]):
+            parameters in a dictionary
+        bcd_rt_list (list[list[str]]):
+            a list of barcodes or RT in format [name, sequence]
+
+    Raises:
+        InvalidNbrLocusException: If the number of available barcodes or RTs is insufficient
+            compared to the total number of loci.
+    """
+    type_bcdrt = "barcodes" if parameters["bcd_rt_file"] == "Barcodes.csv" else "RTs"
+    if len(bcd_rt_list) < parameters["nbr_loci_total"]:
+        raise InvalidNbrLocusException(
+            nbr_locus=parameters["nbr_loci_total"],
+            nbr_bcd_rt=len(bcd_rt_list),
+            type_bcd_rt=type_bcdrt,
+        )
+
 
 class Locus:
     """A class for storing all the information about a specific locus
 
     Attributes:
     -----------
-        locus_n (int): 
+        locus_n (int):
             Locus Number. Defaults to None.
-        chr_name (str): 
+        chr_name (str):
             Chromosome name. Defaults to None.
-        start_seq (int): 
+        start_seq (int):
             Locus start coordinates (in bp). Defaults to None.
-        end_seq (int): 
+        end_seq (int):
             Locus end coordinates (in bp). Defaults to None.
-        primers_univ (list[str]): 
+        primers_univ (list[str]):
             Names and sequences of universal primers in list form. Defaults to None.
-        bcd_locus (str): 
+        bcd_locus (str):
             Barcode or RT name. Defaults to None.
-        seq_probe (list[str]): 
+        seq_probe (list[str]):
             primary probes sequences in list form. Defaults to None.
     """
-    
+
     def __init__(
         self,
         primers_univ: list[str],
@@ -60,7 +85,6 @@ class Locus:
         self.bcd_locus = bcd_locus
         self.seq_probe = seq_probe
 
-
     def add_seq(self, list_seq: list[list[str]]) -> None:
         """Add the list of sequence to the Locus
 
@@ -68,22 +92,24 @@ class Locus:
             list_seq (list[str]): A list of sequence
         """
         self.seq_probe = list_seq
-    
-    def check_nbr_probes(self, list_seq: list[list[int, int, str]]) -> list[list[int, int, str]]:
+
+    def check_nbr_probes(
+        self, list_seq: list[list[int, int, str]]
+    ) -> list[list[int, int, str]]:
         """Checks the number of primary sequences for the locus,
         and mixes and reduces the number of sequences if the maximum limit is reached.
 
         Args:
-            list_seq (list[list[str]): 
+            list_seq (list[list[str]):
                 A list of sequence : [[80000, 80020, 'CGATCGTGATGCTAGCATGT'], ...]
 
         Returns:
-            (list[list[str]): 
+            (list[list[str]):
                 A list of sequence reduced: [[80000, 80020, 'CGATCGTGATGCTAGCATGT'], ...]
         """
         if len(list_seq) > self.nbr_probe_by_locus:
             random.shuffle(list_seq)
-            list_seq = list_seq[:self.nbr_probe_by_locus]
+            list_seq = list_seq[: self.nbr_probe_by_locus]
         return sorted(list_seq)
 
     def recover_genomic_seq(
@@ -93,12 +119,12 @@ class Locus:
         start_lib: int,
         seq_list_reduced: list[list[str]],
     ) -> tuple[list[str], int, int]:
-        """Recover genomic sequences based on locus number ( = coordinates) 
+        """Recover genomic sequences based on locus number ( = coordinates)
 
         Args:
-            locus (int): 
+            locus (int):
                 Locus number
-            seq_list_reduced (list[list[str]]): 
+            seq_list_reduced (list[list[str]]):
                 list of all sequences for the librairy
 
         Returns:
@@ -106,22 +132,33 @@ class Locus:
         """
         if self.design_type == "locus_length":
             # Calculation of start and end coordinates for each locus
-            start_positions = [start_lib + x * self.resolution for x in range(nbr_loci_total)]
-            end_positions = [start_lib + (x + 1) * self.resolution for x in range(nbr_loci_total)]
+            start_positions = [
+                start_lib + x * self.resolution for x in range(nbr_loci_total)
+            ]
+            end_positions = [
+                start_lib + (x + 1) * self.resolution for x in range(nbr_loci_total)
+            ]
             temp = []
             for seq in seq_list_reduced:
-                if (start_positions[locus-1] <= seq[0]) and (seq[1] < end_positions[locus-1]):
+                if (start_positions[locus - 1] <= seq[0]) and (
+                    seq[1] < end_positions[locus - 1]
+                ):
                     temp.append(seq)
                 else:
                     pass
 
             final_seq_list = self.check_nbr_probes(temp)
-            start = start_positions[locus-1] # to be more precise : final_seq_list[0][0]
-            end = end_positions[locus-1] # to be more precise : final_seq_list[-1][1]
+            start = start_positions[
+                locus - 1
+            ]  # to be more precise : final_seq_list[0][0]
+            end = end_positions[locus - 1]  # to be more precise : final_seq_list[-1][1]
             return [x[2] for x in final_seq_list], start, end
 
         elif self.design_type == "nbr_probes":
-            final_seq_list = seq_list_reduced[(locus-1)*self.nbr_probe_by_locus:(locus*self.nbr_probe_by_locus)]
+            final_seq_list = seq_list_reduced[
+                (locus - 1)
+                * self.nbr_probe_by_locus : (locus * self.nbr_probe_by_locus)
+            ]
             start = final_seq_list[0][0]
             end = final_seq_list[-1][1]
             final_seq = [x[2] for x in final_seq_list]
